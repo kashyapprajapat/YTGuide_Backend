14a13815bd0d53a63ddb04d8c1e2f9a0f69fbcf8
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-07-16 19:15:34 +0530

    feat:enhance API security with helmet, xss-clean, and hpp,express-slow-down  middleware - Added helmet to set secure HTTP headers - Integrated xss-clean to sanitize user input and prevent XSS attacks - Used hpp to protect against HTTP parameter pollution - Maintained existing rate limiting with express-rate-limit - Prepared  support for express-slow-down

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const slowDown = require('express-slow-down');
 const { getVideoDescriptions } = require('./helpers/youtubeHelper');
 const { analyzeWithGemini } = require('./helpers/geminiHelper');
 
+app.use(cors());
 app.use(express.json());
+app.use(helmet());          
+app.use(xss());              
+app.use(hpp());             
+
+const limiter = rateLimit({
+  windowMs: 1000, 
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: {
+    status: 'fail',
+    message: 'Too many requests from this IP, please try again later.'
+  }
+});
+
+const speedLimiter = slowDown({
+  windowMs: 60 * 1000, 
+  delayAfter: 5,       
+  delayMs: 700       
+});
+
+
+app.use(limiter);
+app.use(speedLimiter);
+
 const PORT = process.env.PORT || 7000;
+
+
 
 
 // YTGuide Version 1️⃣
@@ -22,7 +54,6 @@ app.post('/api/v1/youtube-urls', async (req, res) => {
   try {
     const descriptions = await getVideoDescriptions(urls);
     const geminiResponse = await analyzeWithGemini(goal, descriptions);
-   
 
     res.status(200).json({
       status: 'success',
