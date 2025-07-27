aab89b53f7a8f3e3bdb9c2407177df817608e7fa
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-07-27 11:10:10 +0530

    feat(index):enable CORS for localhost and yt-guide-one.vercel.app

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const { getVideoDescriptions } = require('./helpers/youtubeHelper');
 const { analyzeWithGemini } = require('./helpers/geminiHelper');
 const { analyzeWithGroq } = require('./helpers/groqHelper');
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://yt-guide-one.vercel.app'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(helmet());                       
 app.use(hpp());             
